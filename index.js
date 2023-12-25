0343fea37fc7f4f7285aff125f4493f56e61afd1
taimoorali-code <140385448+taimoorali-code@users.noreply.github.com>
2023-12-25 14:38:26 +0500

    data modeling of ecomerce project

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 
-require('dotenv').config()
-const express = require('express')
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/abcd', (req, res)=>{
-    res.send('this is first request')
-
-})
-app.get('/taimoor', (req, res)=>{
-    res.send('this is first request')
-
-})
-app.get('/hello', (req, res)=>{
-    res.send('this is second request')
-
-})
 app.listen(process.env.port, () => {
   console.log(`Example app listening on port ${process.env.port}`)
 })
