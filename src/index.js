ff4dd7cad0d4b17c82317fca3e1795b7ea8dbc02
taimoorali-code <140385448+taimoorali-code@users.noreply.github.com>
2023-12-27 07:28:15 +0500

    creating user and video models and learn about hooks and jwt

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import dotenv from 'dotenv'
-import connectDb from './db/index.js';
-
-
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import {app} from './app.js'
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
-connectDb()
+
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
